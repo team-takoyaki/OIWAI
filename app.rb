ceb6fc8c938423e0ad108ec13982230f508fb6c3
dekokun <dekokun@hatena.ne.jp>
2015-09-12 12:31:47 +0900

    おめでとうカウント入れた

--- app.rb
+++ app.rb
@@ -12,12 +12,14 @@ get '/list' do
     {
       id: 1,
       image_url: 'http://www.shogi.or.jp/player/ph_kishi/segawa.jpg',
-      title: '瀬川くん障害対応お疲れ様'
+      title: '瀬川くん障害対応お疲れ様',
+      omedetou_count: 10
     },
     {
       id: 2,
       image_url: 'http://aya-web.com/img/ayakasegawa.jpg',
-      title: 'ほげほげ'
+      title: 'ほげほげ',
+      omedetou_count: 100
     },
   ]
   oiwais.to_json
