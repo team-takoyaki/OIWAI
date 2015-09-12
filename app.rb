d6006b7b668e346f9170f41a4a94d43b7c33cd84
dekokun <dekokun@hatena.ne.jp>
2015-09-12 11:59:46 +0900

    画像を存在するものに

--- app.rb
+++ app.rb
@@ -11,12 +11,12 @@ get '/list' do
   [
     {
       id: 1,
-      image_url: 'http://お祝い祭り/image/1.gif',
+      image_url: 'http://www.shogi.or.jp/player/ph_kishi/segawa.jpg',
       title: '瀬川くん障害対応お疲れ様'
     },
     {
       id: 2,
-      image_url: 'http://お祝い祭り/image/2.gif',
+      image_url: 'http://aya-web.com/img/ayakasegawa.jpg',
       title: 'ほげほげ'
     },
   ].to_json
