e1ee5ce40ba1597f6bb222186a8031181e541237
dekokun <dekokun@hatena.ne.jp>
2015-09-12 11:35:33 +0900

    API作った

--- app.rb
+++ app.rb
@@ -1,6 +1,22 @@
 require 'bundler/setup'
 require 'sinatra'
+require 'json'
 
 get '/' do
   redirect to('/index.html')
+end
+
+get '/list' do
+  [
+    {
+      id: 1,
+      image_url: 'http://お祝い祭り/image/1.gif',
+      title: '瀬川くん障害対応お疲れ様'
+    },
+    {
+      id: 2,
+      image_url: 'http://お祝い祭り/image/2.gif',
+      title: 'ほげほげ'
+    },
+  ].to_json
 end
